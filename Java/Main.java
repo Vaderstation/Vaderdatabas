dab630b09189669a34a9b99ec552c69df78c08cd
AxelJohnssonn <mr.axjo3@gmail.com>
2022-12-09 12:55:57 +0100

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -10,18 +10,14 @@ public class Main {
  
         db.connect();
 
-        System.out.println("Vadeer vaader VAADER");
+        System.out.println("Vader_StationV2");
 
         Map<String, Integer> dataSet = db.resultSetToMap(db.getMeasureValue(1, 1));
 
-<<<<<<< HEAD
-        Interface interFace = new Interface(dataSet);
-=======
         //Interface interFace = new Interface(dataSet);
 
         View veiw = new View(dataSet);
 
 
->>>>>>> 296666146cdbdf2df0ce4c9f2d56f3ac945d22a8
     }
 }
